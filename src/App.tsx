5fc12b42645000148c64a244abc3e2212abb2d89
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-01-10 19:59:32 -0500

    react-github-fork-banner added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { GithubForkBanner } from 'react-github-fork-banner'
+
 import RandomUser from './components/random-user'
 
 function App() {
-  return <RandomUser />
+  return (
+    <>
+      <GithubForkBanner
+        direction="right"
+        size={92}
+        animation="ears"
+        customHref="https://github.com/abrahamcalsin/random-user-api"
+        bannerColor="#183153"
+        octoColor="#F7F9FE"
+      />
+
+      <div className="flex px-3 min-h-screen">
+        <RandomUser />
+      </div>
+    </>
+  )
 }
 
 export default App
